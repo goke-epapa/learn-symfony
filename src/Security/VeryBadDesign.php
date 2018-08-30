56ec51aa6a75083670a41bcd7753dc1a6245aa74
Adegoke Obasa <adegokeobasa@gmail.com>
2018-08-31 01:18:54 +0200

    Service aliases

--- src/Security/VeryBadDesign.php
+++ src/Security/VeryBadDesign.php
@@ -23,6 +23,6 @@ class VeryBadDesign implements ContainerAwareInterface
      */
     public function setContainer(ContainerInterface $container = null)
     {
-        $container->get(Greeting::class);
+        $container->get('app.greeting');
     }
 }
