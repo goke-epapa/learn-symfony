8e930127765abc0abd3174a78f1071d7ad69ba87
Adegoke Obasa <adegokeobasa@gmail.com>
2018-09-06 21:49:15 +0200

    Doctrine: generate Entity and create and run migrations

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ * @ORM\Table()
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=280)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
