ad96d540ae606678f2d7f9cec245bc8e036b7420
Adegoke Obasa <adegokeobasa@gmail.com>
2018-09-09 23:06:37 +0200

    assets: add base html template add app.css to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,11 @@ Encore
         './node_modules/jquery/dist/jquery.slim.js',
         './node_modules/popper.js/dist/popper.min.js',
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/holderjs/holder.min.js'
+        './node_modules/holderjs/holder.min.js',
     ])
     .addStyleEntry('css/app', [
-        './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './assets/css/app.css'
     ])
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
